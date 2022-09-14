b81de36d241866bf5ea17177618407a33ce1c377
gagliardetto <gagliardetto@users.noreply.github.com>
2022-09-15 01:34:09 +0200

    Add sbf/ins_builder.go

--- pkg/sbf/ebpf.go
+++ pkg/sbf/ebpf.go
@@ -1,5 +1,7 @@
 package sbf
 
+import "fmt"
+
 const (
 	// SBF version flag
 	EF_SBF_V2 = 0x20
@@ -26,7 +28,7 @@ const (
 )
 
 const (
-	// 	Memory map regions virtual addresses need to be (1 << VIRTUAL_ADDRESS_BITS) bytes apart.
+	// Memory map regions virtual addresses need to be (1 << VIRTUAL_ADDRESS_BITS) bytes apart.
 	// Also the region at index 0 should be skipped to catch NULL ptr accesses.
 
 	// Start of the program bits (text and ro segments) in the memory map
@@ -38,3 +40,422 @@ const (
 	// Start of the input buffers in the memory map
 	MM_INPUT_START = 0x400000000
 )
+
+const (
+	// eBPF op codes.
+	// See also https://www.kernel.org/doc/Documentation/networking/filter.txt
+
+	// Three least significant bits are operation class:
+	// BPF operation class: load from immediate.
+	BPF_LD uint8 = 0x00
+	// BPF operation class: load from register.
+	BPF_LDX uint8 = 0x01
+	// BPF operation class: store immediate.
+	BPF_ST uint8 = 0x02
+	// BPF operation class: store value from register.
+	BPF_STX uint8 = 0x03
+	// BPF operation class: 32 bits arithmetic operation.
+	BPF_ALU uint8 = 0x04
+	// BPF operation class: jump.
+	BPF_JMP uint8 = 0x05
+	// [ class 6 unused, reserved for future use ]
+	// BPF operation class: 64 bits arithmetic operation.
+	BPF_ALU64 uint8 = 0x07
+)
+
+const (
+	// For load and store instructions:
+	// +------------+--------+------------+
+	// |   3 bits   | 2 bits |   3 bits   |
+	// |    mode    |  size  | insn class |
+	// +------------+--------+------------+
+	// (MSB)                          (LSB)
+
+	// Size modifiers:
+	// BPF size modifier: word (4 bytes).
+	BPF_W uint8 = 0x00
+	// BPF size modifier: half-word (2 bytes).
+	BPF_H uint8 = 0x08
+	// BPF size modifier: byte (1 byte).
+	BPF_B uint8 = 0x10
+	// BPF size modifier: double word (8 bytes).
+	BPF_DW uint8 = 0x18
+
+	// Mode modifiers:
+	// BPF mode modifier: immediate value.
+	BPF_IMM uint8 = 0x00
+	// BPF mode modifier: absolute load.
+	BPF_ABS uint8 = 0x20
+	// BPF mode modifier: indirect load.
+	BPF_IND uint8 = 0x40
+	// BPF mode modifier: load from / store to memory.
+	BPF_MEM uint8 = 0x60
+	// [ 0x80 reserved ]
+	// [ 0xa0 reserved ]
+	// BPF mode modifier: exclusive add.
+	BPF_XADD uint8 = 0xc0
+)
+
+const (
+	// For arithmetic (BPF_ALU/BPF_ALU64) and jump (BPF_JMP) instructions:
+	// +----------------+--------+--------+
+	// |     4 bits     |1 b.|   3 bits   |
+	// | operation code | src| insn class |
+	// +----------------+----+------------+
+	// (MSB)                          (LSB)
+
+	// Source modifiers:
+	// BPF source operand modifier: 32-bit immediate value.
+	BPF_K uint8 = 0x00
+	// BPF source operand modifier: `src` register.
+	BPF_X uint8 = 0x08
+
+	// Operation codes -- BPF_ALU or BPF_ALU64 classes:
+	// BPF ALU/ALU64 operation code: addition.
+	BPF_ADD uint8 = 0x00
+	// BPF ALU/ALU64 operation code: subtraction.
+	BPF_SUB uint8 = 0x10
+	// BPF ALU/ALU64 operation code: multiplication.
+	BPF_MUL uint8 = 0x20
+	// BPF ALU/ALU64 operation code: division.
+	BPF_DIV uint8 = 0x30
+	// BPF ALU/ALU64 operation code: or.
+	BPF_OR uint8 = 0x40
+	// BPF ALU/ALU64 operation code: and.
+	BPF_AND uint8 = 0x50
+	// BPF ALU/ALU64 operation code: left shift.
+	BPF_LSH uint8 = 0x60
+	// BPF ALU/ALU64 operation code: right shift.
+	BPF_RSH uint8 = 0x70
+	// BPF ALU/ALU64 operation code: negation.
+	BPF_NEG uint8 = 0x80
+	// BPF ALU/ALU64 operation code: modulus.
+	BPF_MOD uint8 = 0x90
+	// BPF ALU/ALU64 operation code: exclusive or.
+	BPF_XOR uint8 = 0xa0
+	// BPF ALU/ALU64 operation code: move.
+	BPF_MOV uint8 = 0xb0
+	// BPF ALU/ALU64 operation code: sign extending right shift.
+	BPF_ARSH uint8 = 0xc0
+	// BPF ALU/ALU64 operation code: endianness conversion.
+	BPF_END uint8 = 0xd0
+	// BPF ALU/ALU64 operation code: signed division.
+	BPF_SDIV uint8 = 0xe0
+)
+
+const (
+	// Operation codes -- BPF_JMP class:
+	// BPF JMP operation code: jump.
+	BPF_JA uint8 = 0x00
+	// BPF JMP operation code: jump if equal.
+	BPF_JEQ uint8 = 0x10
+	// BPF JMP operation code: jump if greater than.
+	BPF_JGT uint8 = 0x20
+	// BPF JMP operation code: jump if greater or equal.
+	BPF_JGE uint8 = 0x30
+	// BPF JMP operation code: jump if `src` & `reg`.
+	BPF_JSET uint8 = 0x40
+	// BPF JMP operation code: jump if not equal.
+	BPF_JNE uint8 = 0x50
+	// BPF JMP operation code: jump if greater than (signed).
+	BPF_JSGT uint8 = 0x60
+	// BPF JMP operation code: jump if greater or equal (signed).
+	BPF_JSGE uint8 = 0x70
+	// BPF JMP operation code: syscall function call.
+	BPF_CALL uint8 = 0x80
+	// BPF JMP operation code: return from program.
+	BPF_EXIT uint8 = 0x90
+	// BPF JMP operation code: jump if lower than.
+	BPF_JLT uint8 = 0xa0
+	// BPF JMP operation code: jump if lower or equal.
+	BPF_JLE uint8 = 0xb0
+	// BPF JMP operation code: jump if lower than (signed).
+	BPF_JSLT uint8 = 0xc0
+	// BPF JMP operation code: jump if lower or equal (signed).
+	BPF_JSLE uint8 = 0xd0
+)
+
+const (
+	// Op codes
+	// (Following operation names are not “official”, but may be proper to rbpf; Linux kernel only
+	// combines above flags and does not attribute a name per operation.)
+
+	// BPF opcode: `ldabsb src, dst, imm`.
+	LD_ABS_B uint8 = BPF_LD | BPF_ABS | BPF_B
+	// BPF opcode: `ldabsh src, dst, imm`.
+	LD_ABS_H uint8 = BPF_LD | BPF_ABS | BPF_H
+	// BPF opcode: `ldabsw src, dst, imm`.
+	LD_ABS_W uint8 = BPF_LD | BPF_ABS | BPF_W
+	// BPF opcode: `ldabsdw src, dst, imm`.
+	LD_ABS_DW uint8 = BPF_LD | BPF_ABS | BPF_DW
+	// BPF opcode: `ldindb src, dst, imm`.
+	LD_IND_B uint8 = BPF_LD | BPF_IND | BPF_B
+	// BPF opcode: `ldindh src, dst, imm`.
+	LD_IND_H uint8 = BPF_LD | BPF_IND | BPF_H
+	// BPF opcode: `ldindw src, dst, imm`.
+	LD_IND_W uint8 = BPF_LD | BPF_IND | BPF_W
+	// BPF opcode: `ldinddw src, dst, imm`.
+	LD_IND_DW uint8 = BPF_LD | BPF_IND | BPF_DW
+
+	// BPF opcode: `lddw dst, imm` // `dst = imm`.
+	LD_DW_IMM uint8 = BPF_LD | BPF_IMM | BPF_DW
+	// BPF opcode: `ldxb dst, [src + off]` // `dst = (src + off) as u8`.
+	LD_B_REG uint8 = BPF_LDX | BPF_MEM | BPF_B
+	// BPF opcode: `ldxh dst, [src + off]` // `dst = (src + off) as u16`.
+	LD_H_REG uint8 = BPF_LDX | BPF_MEM | BPF_H
+	// BPF opcode: `ldxw dst, [src + off]` // `dst = (src + off) as u32`.
+	LD_W_REG uint8 = BPF_LDX | BPF_MEM | BPF_W
+	// BPF opcode: `ldxdw dst, [src + off]` // `dst = (src + off) as u64`.
+	LD_DW_REG uint8 = BPF_LDX | BPF_MEM | BPF_DW
+	// BPF opcode: `stb [dst + off], imm` // `(dst + offset) as u8 = imm`.
+	ST_B_IMM uint8 = BPF_ST | BPF_MEM | BPF_B
+	// BPF opcode: `sth [dst + off], imm` // `(dst + offset) as u16 = imm`.
+	ST_H_IMM uint8 = BPF_ST | BPF_MEM | BPF_H
+	// BPF opcode: `stw [dst + off], imm` // `(dst + offset) as u32 = imm`.
+	ST_W_IMM uint8 = BPF_ST | BPF_MEM | BPF_W
+	// BPF opcode: `stdw [dst + off], imm` // `(dst + offset) as u64 = imm`.
+	ST_DW_IMM uint8 = BPF_ST | BPF_MEM | BPF_DW
+	// BPF opcode: `stxb [dst + off], src` // `(dst + offset) as u8 = src`.
+	ST_B_REG uint8 = BPF_STX | BPF_MEM | BPF_B
+	// BPF opcode: `stxh [dst + off], src` // `(dst + offset) as u16 = src`.
+	ST_H_REG uint8 = BPF_STX | BPF_MEM | BPF_H
+	// BPF opcode: `stxw [dst + off], src` // `(dst + offset) as u32 = src`.
+	ST_W_REG uint8 = BPF_STX | BPF_MEM | BPF_W
+	// BPF opcode: `stxdw [dst + off], src` // `(dst + offset) as u64 = src`.
+	ST_DW_REG uint8 = BPF_STX | BPF_MEM | BPF_DW
+
+	// BPF opcode: `stxxaddw [dst + off], src`.
+	ST_W_XADD uint8 = BPF_STX | BPF_XADD | BPF_W
+	// BPF opcode: `stxxadddw [dst + off], src`.
+	ST_DW_XADD uint8 = BPF_STX | BPF_XADD | BPF_DW
+
+	// BPF opcode: `add32 dst, imm` // `dst += imm`.
+	ADD32_IMM uint8 = BPF_ALU | BPF_K | BPF_ADD
+	// BPF opcode: `add32 dst, src` // `dst += src`.
+	ADD32_REG uint8 = BPF_ALU | BPF_X | BPF_ADD
+	// BPF opcode: `sub32 dst, imm` // `dst -= imm`.
+	SUB32_IMM uint8 = BPF_ALU | BPF_K | BPF_SUB
+	// BPF opcode: `sub32 dst, src` // `dst -= src`.
+	SUB32_REG uint8 = BPF_ALU | BPF_X | BPF_SUB
+	// BPF opcode: `mul32 dst, imm` // `dst *= imm`.
+	MUL32_IMM uint8 = BPF_ALU | BPF_K | BPF_MUL
+	// BPF opcode: `mul32 dst, src` // `dst *= src`.
+	MUL32_REG uint8 = BPF_ALU | BPF_X | BPF_MUL
+	// BPF opcode: `div32 dst, imm` // `dst /= imm`.
+	DIV32_IMM uint8 = BPF_ALU | BPF_K | BPF_DIV
+	// BPF opcode: `div32 dst, src` // `dst /= src`.
+	DIV32_REG uint8 = BPF_ALU | BPF_X | BPF_DIV
+	// BPF opcode: `or32 dst, imm` // `dst |= imm`.
+	OR32_IMM uint8 = BPF_ALU | BPF_K | BPF_OR
+	// BPF opcode: `or32 dst, src` // `dst |= src`.
+	OR32_REG uint8 = BPF_ALU | BPF_X | BPF_OR
+	// BPF opcode: `and32 dst, imm` // `dst &= imm`.
+	AND32_IMM uint8 = BPF_ALU | BPF_K | BPF_AND
+	// BPF opcode: `and32 dst, src` // `dst &= src`.
+	AND32_REG uint8 = BPF_ALU | BPF_X | BPF_AND
+	// BPF opcode: `lsh32 dst, imm` // `dst <<= imm`.
+	LSH32_IMM uint8 = BPF_ALU | BPF_K | BPF_LSH
+	// BPF opcode: `lsh32 dst, src` // `dst <<= src`.
+	LSH32_REG uint8 = BPF_ALU | BPF_X | BPF_LSH
+	// BPF opcode: `rsh32 dst, imm` // `dst >>= imm`.
+	RSH32_IMM uint8 = BPF_ALU | BPF_K | BPF_RSH
+	// BPF opcode: `rsh32 dst, src` // `dst >>= src`.
+	RSH32_REG uint8 = BPF_ALU | BPF_X | BPF_RSH
+	// BPF opcode: `neg32 dst` // `dst = -dst`.
+	NEG32 uint8 = BPF_ALU | BPF_NEG
+	// BPF opcode: `mod32 dst, imm` // `dst %= imm`.
+	MOD32_IMM uint8 = BPF_ALU | BPF_K | BPF_MOD
+	// BPF opcode: `mod32 dst, src` // `dst %= src`.
+	MOD32_REG uint8 = BPF_ALU | BPF_X | BPF_MOD
+	// BPF opcode: `xor32 dst, imm` // `dst ^= imm`.
+	XOR32_IMM uint8 = BPF_ALU | BPF_K | BPF_XOR
+	// BPF opcode: `xor32 dst, src` // `dst ^= src`.
+	XOR32_REG uint8 = BPF_ALU | BPF_X | BPF_XOR
+	// BPF opcode: `mov32 dst, imm` // `dst = imm`.
+	MOV32_IMM uint8 = BPF_ALU | BPF_K | BPF_MOV
+	// BPF opcode: `mov32 dst, src` // `dst = src`.
+	MOV32_REG uint8 = BPF_ALU | BPF_X | BPF_MOV
+	// BPF opcode: `arsh32 dst, imm` // `dst >>= imm (arithmetic)`.
+	///
+	// <https://en.wikipedia.org/wiki/Arithmetic_shift>
+	ARSH32_IMM uint8 = BPF_ALU | BPF_K | BPF_ARSH
+	// BPF opcode: `arsh32 dst, src` // `dst >>= src (arithmetic)`.
+	///
+	// <https://en.wikipedia.org/wiki/Arithmetic_shift>
+	ARSH32_REG uint8 = BPF_ALU | BPF_X | BPF_ARSH
+	// BPF opcode: `sdiv32 dst, imm` // `dst s/= imm`.
+	SDIV32_IMM uint8 = BPF_ALU | BPF_K | BPF_SDIV
+	// BPF opcode: `sdiv32 dst, src` // `dst s/= src`.
+	SDIV32_REG uint8 = BPF_ALU | BPF_X | BPF_SDIV
+
+	// BPF opcode: `le dst` // `dst = htole<imm>(dst), with imm in {16, 32, 64}`.
+	LE uint8 = BPF_ALU | BPF_K | BPF_END
+	// BPF opcode: `be dst` // `dst = htobe<imm>(dst), with imm in {16, 32, 64}`.
+	BE uint8 = BPF_ALU | BPF_X | BPF_END
+
+	// BPF opcode: `add64 dst, imm` // `dst += imm`.
+	ADD64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_ADD
+	// BPF opcode: `add64 dst, src` // `dst += src`.
+	ADD64_REG uint8 = BPF_ALU64 | BPF_X | BPF_ADD
+	// BPF opcode: `sub64 dst, imm` // `dst -= imm`.
+	SUB64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_SUB
+	// BPF opcode: `sub64 dst, src` // `dst -= src`.
+	SUB64_REG uint8 = BPF_ALU64 | BPF_X | BPF_SUB
+	// BPF opcode: `div64 dst, imm` // `dst /= imm`.
+	MUL64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_MUL
+	// BPF opcode: `div64 dst, src` // `dst /= src`.
+	MUL64_REG uint8 = BPF_ALU64 | BPF_X | BPF_MUL
+	// BPF opcode: `div64 dst, imm` // `dst /= imm`.
+	DIV64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_DIV
+	// BPF opcode: `div64 dst, src` // `dst /= src`.
+	DIV64_REG uint8 = BPF_ALU64 | BPF_X | BPF_DIV
+	// BPF opcode: `or64 dst, imm` // `dst |= imm`.
+	OR64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_OR
+	// BPF opcode: `or64 dst, src` // `dst |= src`.
+	OR64_REG uint8 = BPF_ALU64 | BPF_X | BPF_OR
+	// BPF opcode: `and64 dst, imm` // `dst &= imm`.
+	AND64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_AND
+	// BPF opcode: `and64 dst, src` // `dst &= src`.
+	AND64_REG uint8 = BPF_ALU64 | BPF_X | BPF_AND
+	// BPF opcode: `lsh64 dst, imm` // `dst <<= imm`.
+	LSH64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_LSH
+	// BPF opcode: `lsh64 dst, src` // `dst <<= src`.
+	LSH64_REG uint8 = BPF_ALU64 | BPF_X | BPF_LSH
+	// BPF opcode: `rsh64 dst, imm` // `dst >>= imm`.
+	RSH64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_RSH
+	// BPF opcode: `rsh64 dst, src` // `dst >>= src`.
+	RSH64_REG uint8 = BPF_ALU64 | BPF_X | BPF_RSH
+	// BPF opcode: `neg64 dst, imm` // `dst = -dst`.
+	NEG64 uint8 = BPF_ALU64 | BPF_NEG
+	// BPF opcode: `mod64 dst, imm` // `dst %= imm`.
+	MOD64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_MOD
+	// BPF opcode: `mod64 dst, src` // `dst %= src`.
+	MOD64_REG uint8 = BPF_ALU64 | BPF_X | BPF_MOD
+	// BPF opcode: `xor64 dst, imm` // `dst ^= imm`.
+	XOR64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_XOR
+	// BPF opcode: `xor64 dst, src` // `dst ^= src`.
+	XOR64_REG uint8 = BPF_ALU64 | BPF_X | BPF_XOR
+	// BPF opcode: `mov64 dst, imm` // `dst = imm`.
+	MOV64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_MOV
+	// BPF opcode: `mov64 dst, src` // `dst = src`.
+	MOV64_REG uint8 = BPF_ALU64 | BPF_X | BPF_MOV
+	// BPF opcode: `arsh64 dst, imm` // `dst >>= imm (arithmetic)`.
+	///
+	// <https://en.wikipedia.org/wiki/Arithmetic_shift>
+	ARSH64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_ARSH
+	// BPF opcode: `arsh64 dst, src` // `dst >>= src (arithmetic)`.
+	///
+	// <https://en.wikipedia.org/wiki/Arithmetic_shift>
+	ARSH64_REG uint8 = BPF_ALU64 | BPF_X | BPF_ARSH
+	// BPF opcode: `sdiv64 dst, imm` // `dst s/= imm`.
+	SDIV64_IMM uint8 = BPF_ALU64 | BPF_K | BPF_SDIV
+	// BPF opcode: `sdiv64 dst, src` // `dst s/= src`.
+	SDIV64_REG uint8 = BPF_ALU64 | BPF_X | BPF_SDIV
+
+	// BPF opcode: `ja +off` // `PC += off`.
+	JA uint8 = BPF_JMP | BPF_JA
+	// BPF opcode: `jeq dst, imm, +off` // `PC += off if dst == imm`.
+	JEQ_IMM uint8 = BPF_JMP | BPF_K | BPF_JEQ
+	// BPF opcode: `jeq dst, src, +off` // `PC += off if dst == src`.
+	JEQ_REG uint8 = BPF_JMP | BPF_X | BPF_JEQ
+	// BPF opcode: `jgt dst, imm, +off` // `PC += off if dst > imm`.
+	JGT_IMM uint8 = BPF_JMP | BPF_K | BPF_JGT
+	// BPF opcode: `jgt dst, src, +off` // `PC += off if dst > src`.
+	JGT_REG uint8 = BPF_JMP | BPF_X | BPF_JGT
+	// BPF opcode: `jge dst, imm, +off` // `PC += off if dst >= imm`.
+	JGE_IMM uint8 = BPF_JMP | BPF_K | BPF_JGE
+	// BPF opcode: `jge dst, src, +off` // `PC += off if dst >= src`.
+	JGE_REG uint8 = BPF_JMP | BPF_X | BPF_JGE
+	// BPF opcode: `jlt dst, imm, +off` // `PC += off if dst < imm`.
+	JLT_IMM uint8 = BPF_JMP | BPF_K | BPF_JLT
+	// BPF opcode: `jlt dst, src, +off` // `PC += off if dst < src`.
+	JLT_REG uint8 = BPF_JMP | BPF_X | BPF_JLT
+	// BPF opcode: `jle dst, imm, +off` // `PC += off if dst <= imm`.
+	JLE_IMM uint8 = BPF_JMP | BPF_K | BPF_JLE
+	// BPF opcode: `jle dst, src, +off` // `PC += off if dst <= src`.
+	JLE_REG uint8 = BPF_JMP | BPF_X | BPF_JLE
+	// BPF opcode: `jset dst, imm, +off` // `PC += off if dst & imm`.
+	JSET_IMM uint8 = BPF_JMP | BPF_K | BPF_JSET
+	// BPF opcode: `jset dst, src, +off` // `PC += off if dst & src`.
+	JSET_REG uint8 = BPF_JMP | BPF_X | BPF_JSET
+	// BPF opcode: `jne dst, imm, +off` // `PC += off if dst != imm`.
+	JNE_IMM uint8 = BPF_JMP | BPF_K | BPF_JNE
+	// BPF opcode: `jne dst, src, +off` // `PC += off if dst != src`.
+	JNE_REG uint8 = BPF_JMP | BPF_X | BPF_JNE
+	// BPF opcode: `jsgt dst, imm, +off` // `PC += off if dst > imm (signed)`.
+	JSGT_IMM uint8 = BPF_JMP | BPF_K | BPF_JSGT
+	// BPF opcode: `jsgt dst, src, +off` // `PC += off if dst > src (signed)`.
+	JSGT_REG uint8 = BPF_JMP | BPF_X | BPF_JSGT
+	// BPF opcode: `jsge dst, imm, +off` // `PC += off if dst >= imm (signed)`.
+	JSGE_IMM uint8 = BPF_JMP | BPF_K | BPF_JSGE
+	// BPF opcode: `jsge dst, src, +off` // `PC += off if dst >= src (signed)`.
+	JSGE_REG uint8 = BPF_JMP | BPF_X | BPF_JSGE
+	// BPF opcode: `jslt dst, imm, +off` // `PC += off if dst < imm (signed)`.
+	JSLT_IMM uint8 = BPF_JMP | BPF_K | BPF_JSLT
+	// BPF opcode: `jslt dst, src, +off` // `PC += off if dst < src (signed)`.
+	JSLT_REG uint8 = BPF_JMP | BPF_X | BPF_JSLT
+	// BPF opcode: `jsle dst, imm, +off` // `PC += off if dst <= imm (signed)`.
+	JSLE_IMM uint8 = BPF_JMP | BPF_K | BPF_JSLE
+	// BPF opcode: `jsle dst, src, +off` // `PC += off if dst <= src (signed)`.
+	JSLE_REG uint8 = BPF_JMP | BPF_X | BPF_JSLE
+
+	// BPF opcode: `call imm` // syscall function call to syscall with key `imm`.
+	CALL_IMM uint8 = BPF_JMP | BPF_CALL
+	// BPF opcode: tail call.
+	CALL_REG uint8 = BPF_JMP | BPF_X | BPF_CALL
+	// BPF opcode: `exit` // `return r0`.
+	EXIT uint8 = BPF_JMP | BPF_EXIT
+
+	// Used in JIT
+	// Mask to extract the operation class from an operation code.
+	BPF_CLS_MASK = 0x07
+	// Mask to extract the arithmetic operation code from an instruction operation code.
+	BPF_ALU_OP_MASK = 0xf0
+)
+
+type Insn struct {
+	// Instruction pointer.
+	Ptr uint64
+
+	// Operation code.
+	Opc uint8
+
+	// Destination register operand.
+	Dst uint8
+
+	// Source register operand.
+	Src uint8
+
+	// Offset operand.
+	Off int16
+
+	// Immediate value operand.
+	Imm int64
+}
+
+func (i Insn) String() string {
+	return fmt.Sprintf(
+		"Insn { ptr: 0x%x, opc: 0x%x, dst: %v, src: %v, off: 0x%x, imm: 0x%x }",
+		i.Ptr, i.Opc, i.Dst, i.Src, i.Off, i.Imm,
+	)
+}
+
+// TODO: rename to `ToArray`
+func (insn *Insn) to_array() [INSN_SIZE]uint8 {
+	var bytes [8]uint8
+	bytes[0] = insn.Opc
+	bytes[1] = insn.Src<<4 | insn.Dst
+	bytes[2] = uint8(insn.Off & 0xff)
+	bytes[3] = uint8(insn.Off >> 8)
+	bytes[4] = uint8(insn.Imm & 0xff)
+	bytes[5] = uint8((insn.Imm & 0xff00) >> 8)
+	bytes[6] = uint8((insn.Imm & 0xff0000) >> 16)
+	bytes[7] = uint8((insn.Imm & 0xff000000) >> 24)
+	return bytes
+}
+
+// TODO: rename to `ToVec`
+func (insn *Insn) to_vec() []uint8 {
+	arr := insn.to_array()
+	return arr[:]
+}
---
+++ pkg/sbf/ins_builder_test.go
@@ -0,0 +1,301 @@
+package sbf
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIns(t *testing.T) {
+	{
+		/// let prog: &[u8] = &[
+		///     0xb7, 0x12, 0x56, 0x34, 0xde, 0xbc, 0x9a, 0x78,
+		///     ];
+		/// let insn = ebpf::Insn {
+		///     ptr: 0x00,
+		///     opc: 0xb7,
+		///     dst: 2,
+		///     src: 1,
+		///     off: 0x3456,
+		///     imm: 0x789abcde
+		/// };
+		/// assert_eq!(insn.to_array(), prog);
+
+		prog := [INSN_SIZE]uint8{0xb7, 0x12, 0x56, 0x34, 0xde, 0xbc, 0x9a, 0x78}
+
+		insn := Insn{
+			Ptr: 0x00,
+			Opc: 0xb7,
+			Dst: 2,
+			Src: 1,
+			Off: 0x3456,
+			Imm: 0x789abcde,
+		}
+
+		require.Equal(t, prog, insn.to_array())
+	}
+}
+func TestCallImmediate(t *testing.T) {
+	program := NewBpfCode()
+	program.Call().SetImm(0x11_22_33_44).Push()
+
+	require.Equal(t, program.Bytes(), []byte{0x85, 0x00, 0x00, 0x00, 0x44, 0x33, 0x22, 0x11})
+}
+func TestExitOperation(t *testing.T) {
+	program := NewBpfCode()
+	program.Exit().Push()
+
+	require.Equal(t, []byte{0x95, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstEqualsSrc(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondEquals, SourceReg).SetDst(0x01).SetSrc(0x02).Push()
+
+	require.Equal(t, []byte{0x1d, 0x21, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstGreaterThanSrc(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondGreater, SourceReg).
+		SetDst(0x03).
+		SetSrc(0x02).
+		Push()
+
+	require.Equal(t, program.Bytes(), []byte{0x2d, 0x23, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
+func TestJumpOnDstGreaterOrEqualsToSrc(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondGreaterEquals, SourceReg).SetDst(0x04).SetSrc(0x01).Push()
+
+	require.Equal(t, program.Bytes(), []byte{0x3d, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
+func TestJumpOnDstLowerThanSrc(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondLower, SourceReg).SetDst(0x03).SetSrc(0x02).Push()
+	require.Equal(t, []byte{0xad, 0x23, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstLowerOrEqualsToSrc(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondLowerEquals, SourceReg).
+		SetDst(0x04).
+		SetSrc(0x01).
+		Push()
+
+	require.Equal(t, []byte{
+		0xbd, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}, program.Bytes())
+}
+func TestJumpOnDstBitAndWithSrcNotEqualZero(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondBitAnd, SourceReg).SetDst(0x05).SetSrc(0x02).Push()
+
+	require.Equal(t, []byte{
+		0x4d, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}, program.Bytes())
+}
+func TestJumpOnDstNotEqualsSrc(t *testing.T) {
+	program := NewBpfCode()
+	program.
+		JumpConditional(CondNotEquals, SourceReg).
+		SetDst(0x03).
+		SetSrc(0x05).
+		Push()
+
+	require.Equal(t, []byte{0x5d, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstGreaterThanSrcSigned(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondGreaterSigned, SourceReg).
+		SetDst(0x04).
+		SetSrc(0x01).
+		Push()
+
+	require.Equal(t, program.Bytes(), []byte{0x6d, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
+func TestJumpOnDstGreaterOrEqualsSrcSigned(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondGreaterEqualsSigned, SourceReg).SetDst(0x01).SetSrc(0x03).Push()
+
+	require.Equal(t, []byte{0x7d, 0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstLowerThanSrcSigned(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondLowerSigned, SourceReg).
+		SetDst(0x04).
+		SetSrc(0x01).
+		Push()
+
+	require.Equal(t, []byte{0xcd, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstLowerOrEqualsSrcSigned(t *testing.T) {
+	program := NewBpfCode()
+	program.
+		JumpConditional(CondLowerEqualsSigned, SourceReg).
+		SetDst(0x01).
+		SetSrc(0x03).
+		Push()
+
+	require.Equal(t, []byte{
+		0xdd, 0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}, program.Bytes())
+}
+func TestJumpToLabel(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpUnconditional().SetOff(0x00_11).Push()
+
+	require.Equal(t, program.Bytes(), []byte{0x05, 0x00, 0x11, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
+func TestJumpOnDstEqualsConst(t *testing.T) {
+	program := NewBpfCode()
+	program.
+		JumpConditional(CondEquals, SourceImm).
+		SetDst(0x01).
+		SetImm(0x00_11_22_33).
+		Push()
+
+	require.Equal(t, program.Bytes(), []byte{0x15, 0x01, 0x00, 0x00, 0x33, 0x22, 0x11, 0x00})
+}
+func TestJumpOnDstGreaterThanConst(t *testing.T) {
+	program := NewBpfCode()
+	program.
+		JumpConditional(CondGreater, SourceImm).
+		SetDst(0x02).
+		SetImm(0x00110011).
+		Push()
+
+	require.Equal(t, []byte{
+		0x25, 0x02, 0x00, 0x00, 0x11, 0x00, 0x11, 0x00,
+	}, program.Bytes())
+}
+func TestJumpOnDstGreaterOrEqualsToConst(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondGreaterEquals, SourceImm).
+		SetDst(0x04).
+		SetImm(0x00_22_11_00).
+		Push()
+
+	require.Equal(
+		t,
+		[]byte{0x35, 0x04, 0x00, 0x00, 0x00, 0x11, 0x22, 0x00},
+		program.Bytes(),
+	)
+}
+func TestJumpOnDstLowerThanConst(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondLower, SourceImm).SetDst(0x02).SetImm(0x00_11_00_11).Push()
+
+	require.Equal(t, []byte{0xa5, 0x02, 0x00, 0x00, 0x11, 0x00, 0x11, 0x00}, program.Bytes())
+}
+func TestJumpOnDstLowerOrEqualsToConst(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondLowerEquals, SourceImm).SetDst(0x04).SetImm(0x00_22_11_00).Push()
+
+	require.Equal(t, []byte{0xb5, 0x04, 0x00, 0x00, 0x00, 0x11, 0x22, 0x00}, program.Bytes())
+}
+func Test_jump_on_dst_bit_and_with_const_not_equal_zero(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondBitAnd, SourceImm).
+		SetDst(0x05).
+		Push()
+
+	require.Equal(t, []byte{0x45, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstNotEqualsConst(t *testing.T) {
+	program := NewBpfCode()
+	program.
+		JumpConditional(CondNotEquals, SourceImm).
+		SetDst(0x03).
+		Push()
+
+	require.Equal(t, []byte{0x55, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstGreaterThanConstSigned(t *testing.T) {
+	var program BpfCode
+	program.JumpConditional(CondGreaterSigned, SourceImm).SetDst(0x04).Push()
+
+	require.Equal(
+		t,
+		[]byte{0x65, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		program.Bytes(),
+	)
+}
+func TestJumpOnDstGreaterOrEqualsSrcSigned2(t *testing.T) {
+	program := NewBpfCode()
+	program.
+		JumpConditional(CondGreaterEqualsSigned, SourceImm).
+		SetDst(0x01).
+		Push()
+
+	require.Equal(t, []byte{0x75, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestJumpOnDstLowerThanConstSigned(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondLowerSigned, SourceImm).SetDst(0x04).Push()
+
+	require.Equal(t, []byte{
+		0xc5, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}, program.Bytes())
+}
+func TestJumpOnDstLowerOrEqualsSrcSigned2(t *testing.T) {
+	program := NewBpfCode()
+	program.JumpConditional(CondLowerEqualsSigned, SourceImm).SetDst(0x01).Push()
+
+	require.Equal(t, []byte{0xd5, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestStoreWordFromDstIntoImmediateAddress(t *testing.T) {
+	program := NewBpfCode()
+	program.Store(MemSizeWord).SetDst(0x01).SetOff(0x0011).SetImm(0x11223344).Push()
+
+	expected := []byte{0x62, 0x01, 0x11, 0x00, 0x44, 0x33, 0x22, 0x11}
+	require.Equal(t, expected, program.Bytes())
+}
+func TestStoreHalfWordFromDstIntoImmediateAddress(t *testing.T) {
+	program := NewBpfCode()
+	program.
+		Store(MemSizeHalfWord).SetDst(0x02).SetOff(0x11_22).Push()
+
+	require.Equal(t, []byte{0x6a, 0x02, 0x22, 0x11, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestStoreByteFromDstIntoImmediateAddress(t *testing.T) {
+	program := BpfCode{}
+	program.Store(MemSizeByte).Push()
+
+	require.Equal(t, program.Bytes(), []byte{0x72, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
+func TestStoreDoubleWordFromDstIntoImmediateAddress(t *testing.T) {
+	program := NewBpfCode()
+	program.Store(MemSizeDoubleWord).Push()
+
+	require.Equal(t, []byte{
+		0x7a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}, program.Bytes())
+}
+func TestStoreWordFromDstIntoSrcAddress(t *testing.T) {
+	program := NewBpfCode()
+	program.
+		StoreX(MemSizeWord).
+		SetDst(0x01).
+		SetSrc(0x02).
+		Push()
+
+	require.Equal(t, []byte{0x63, 0x21, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestStoreHalfWordFromDstIntoSrcAddress(t *testing.T) {
+	program := NewBpfCode()
+	program.StoreX(MemSizeHalfWord).Push()
+
+	require.Equal(t, []byte{
+		0x6b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}, program.Bytes())
+}
+func TestStoreByteFromDstIntoSrcAddress(t *testing.T) {
+	program := NewBpfCode()
+	program.StoreX(MemSizeByte).Push()
+
+	require.Equal(t, []byte{0x73, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, program.Bytes())
+}
+func TestStoreDoubleWordFromDstIntoSrcAddress(t *testing.T) { // line 1103
+	program := NewBpfCode()
+	program.StoreX(MemSizeDoubleWord).Push()
+
+	require.Equal(t, program.Bytes(), []byte{0x7b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
